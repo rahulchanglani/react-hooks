30362e5007e3429224f71dec3b0f8b0fd9b5e6f5
Rahul Changlani <rahul.changlani@gmail.com>
2022-04-15 18:58:05 +0530

    added example for UseCallback hook

--- src/hooks/UseCallback/CallbackTut.js
+++ src/hooks/UseCallback/CallbackTut.js
@@ -1,6 +1,9 @@
 import { useCallback, useState } from "react";
 import Child from "./Child";
 
+// used when fn is passed as prop to child component , it will recreate itself in re-render
+// can store the memoized function in itself 
+
 export default function CallBackTutorial() {
   const [toggle, setToggle] = useState(false);
   const [data, setData] = useState("Callback string in Parent ");
