4634f54a8b606cdf10e78609dde9c69ac46890a1
Rahul Changlani <rahul.changlani@gmail.com>
2022-04-15 18:41:33 +0530

    added example for UseMemo hook

--- src/hooks/UseMemo/MemoTut.js
+++ src/hooks/UseMemo/MemoTut.js
@@ -1,9 +1,51 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState, useMemo } from "react";
 
-const MemoTut = () => {
-    return (
-        null
-    )
+// used for memoized function to reduce algorithmic compution time again
+
+export default function MemoTutorial() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
+
+  const findLongestName = (comments) => {
+    if (!comments) return null;
+
+    let longestName = "";
+    for (let i = 0; i < comments.length; i++) {
+      let currentName = comments[i].name;
+      if (currentName.length > longestName.length) {
+        longestName = currentName;
+      }
+    }
+
+    console.log("THIS WAS COMPUTED");
+
+    return longestName;
+  };
+
+  const getLongestName = useMemo(() => findLongestName(data), [toggle]);
+
+  return (
+    <div className="App">
+      <div> {getLongestName} </div>
+
+      <button
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        {" "}
+        Toggle
+      </button>
+      {toggle && <h1> toggle </h1>}
+    </div>
+  );
 }
-
-export default MemoTut
